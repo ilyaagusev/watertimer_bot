7760f5943b8ecc661bc6d132e1427c0a8174c55b
Ilya Gusev <gusev.ilya.a@gmail.com>
2020-01-20 12:13:37 +0300

    updated config. created timer

--- egg_timer.py
+++ egg_timer.py
@@ -1,10 +1,16 @@
 import config
 import ptbot
+import time
 
 
 def notify(name):
-    print('hello, ', name)
+    bot.send_message(config.CHAT_ID, name) 
+
+
+def create_timer(timeout_secs, callback, callback_arg):
+    time.sleep(timeout_secs)
+    return callback(callback_arg)
 
 
 bot = ptbot.Bot(config.API_KEY)
-bot.create_timer(10, notify, 'Viktor')
+create_timer(10, notify, 'Время вышло!')
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import ptbot
 
 
 def reply_msg(text):
-    bot.send_message('722809054', 'Привет! Ты написал мне: {}'.format(text))
+    bot.send_message(config.CHAT_ID, 'Привет! Ты написал мне: {}'.format(text))
 
 
 bot = ptbot.Bot(config.API_KEY)
-bot.send_message('722809054', 'Бот запущен')
+bot.send_message(config.CHAT_ID, 'Бот запущен')
 message_id = bot.wait_for_msg(reply_msg)
